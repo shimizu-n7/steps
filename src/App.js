deb2c51ea3d446d85d28cbe7d767d411572dd61e
shimizu-n7 <nagashimi11n@gmail.com>
2024-05-12 21:46:01 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  // const step = 2;
+  const [step, SetStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step <= 1) {
+      return;
+    }
+    SetStep((prev) => prev - 1);
+  };
+
+  const handleNext = () => {
+    if (step >= 3) {
+      return;
+    }
+    SetStep((prev) => prev + 1);
+  };
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}{" "}
+      </p>
+
+      <div className="buttons">
+        <button
+          onClick={handlePrevious}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
